5cd26fd455a7b85d26115a325d0189acebe245d5
Ansh Lalwani <57198075+TheDarkFyre@users.noreply.github.com>
2020-04-13 12:26:20 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!-- the original code that came with the project -->
+<!--
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links [Insert Name & Period Here]</p>'; ?> 
+    <ul>
+      <li><a href="index.html">index.html</a></li></br>
+      <li><a href="Lalwani_AboutMe.html">AboutMe.html</a></li></br>
+      <li><a href="Lalwani_Projects.html">Projects.html</a></li></br>
+      <li><a href="Lalwani_Resume.html">Resume.html</a></li></br>
+      <li><a href="Lalwani_ContactMe.html">ContactMe.html</a></li></br>
+    <ul>
+  </body>
+</html> 
+-->
+
+<!-- the code to automatically redirect the webpage to the home page -->
+<meta http-equiv="refresh" content="0; URL='https://html-project.thedarkfyre.repl.co/index.html'"/> 
